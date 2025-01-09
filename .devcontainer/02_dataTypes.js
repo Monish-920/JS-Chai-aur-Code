da2420275e52c0eec007f9cc99b374b7dd910aa3
Monish-920 <121478587+Monish-920@users.noreply.github.com>
2025-01-09 13:47:41 +0000

    datatypes discussed

---
+++ .devcontainer/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use Strict"; // will treat all code as newer version of JavaScript//
+ 
+// alert(3 + 3) // we are using nodeJs evironment not browser
+
+console.log (6 
+    +
+     6) // code readability should be high 
+
+console.log("Monish");
+
+let name = "Monish"
+let age = 25
+let isLoggedIn = true
+
+// number => 2 to the power of 53 
+// bigint => used for very large numbers specially in trading and website like reddit and Facebook//
+// string => "" prefer to use double quotes
+// boolean => true/ false
+// null => standalone value
+// undefined => 
+// symbol => use to identify unique components
+
+
+//object =>
+
+    console.log(typeof undefined);// undefined
+    console.log(typeof null); // object
+    
