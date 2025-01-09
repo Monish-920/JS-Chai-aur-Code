7e8f00969d294f221e45694b283417a7e03587d8
Monish-920 <121478587+Monish-920@users.noreply.github.com>
2025-01-09 13:48:01 +0000

    variables discussed

---
+++ .devcontainer/01_Variables.js
@@ -0,0 +1,17 @@
+const accountNum = 252525;
+let emailId ="Monish@gmail.com";
+var accountPassword ="12345";
+currentCity ="Lakhimpur";
+let accountState;
+
+//accountNum = 262626;// variables declared with const cannot be changed
+
+emailId ="Monis@gmail.com"
+accountPassword ="24689"
+currentCity ="Lucknow"
+console.log(accountNum)
+/*
+prefer not to use var,
+because of isuue in block scope and functional scope.
+*/
+console.table([accountNum, emailId, accountPassword, currentCity, accountState])
