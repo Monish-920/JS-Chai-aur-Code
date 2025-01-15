6605c8fe26653905bf5856e4f55459d8f5f620c3
Monish-920 <121478587+Monish-920@users.noreply.github.com>
2025-01-15 17:31:30 +0000

    We have learned conversion and operation

---
+++ .devcontainer/03_ConversionOperation.js
@@ -0,0 +1,26 @@
+/*let age = false
+
+console.log(typeof age);
+console.log(typeof(age));
+
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let loggedIn = false
+
+let valueInNumber = Number(loggedIn)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+let value = ""
+
+let valueInNumber = Number(value)
+console.log(typeof value);
